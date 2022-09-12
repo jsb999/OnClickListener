adbf6276f76b232d5887f932a28ba892e0344155
jsb999 <90780236+jsb999@users.noreply.github.com>
2022-09-12 12:54:38 -0500

    added functionality

--- app/src/main/java/com/example/onclicklistener/MainActivity.java
+++ app/src/main/java/com/example/onclicklistener/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.onclicklistener;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ToggleButton toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toggle = (ToggleButton) findViewById(R.id.toggleButton);
     }
+    public void onClick(View v){
+        if(toggle.isChecked()){
+            Log.i("logOutput", "toggle is checked on");
+        } else {
+            Log.i("logOutput", "toggle is checked off");
+        }
+    }
+
+
 }
